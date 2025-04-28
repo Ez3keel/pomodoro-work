e8f44d624197d389600fce3b99a521e500bf7cbc
Ezequiel <ezequiel.zunesha@gmail.com>
2025-04-28 16:43:09 -0300

    Att de componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,20 @@
 // Importando o css ex.: import "./App.css";
 import "./styles/theme.css";
 import "./styles/global.css";
+import { Container } from "./components/Container";
+import { Heading } from "./components/heading";
 
 //Se exportar direto na function quando for importar no main tem que colocar o mesmo nome no import import Ex.: { App } from "./App";
 export function App() {
-  console.log("Oi");
   return (
     <>
-      <div className="container-fluid">
-        <div className="container">
-          <div className="content">
-            <section>LOGO</section>
-          </div>
-        </div>
-      </div>
-      <div className="container-fluid">
-        <div className="container">
-          <div className="content">
-            <section>MENU</section>
-          </div>
-        </div>
-      </div>
-      <div className="container-fluid">
-        <div className="container">
-          <div className="content">
-            <section>FORM</section>
-          </div>
-        </div>
-      </div>
-      <div className="container-fluid">
-        <div className="container">
-          <div className="content">
-            <section>FOOTER</section>
-          </div>
-        </div>
-      </div>
+      <Container>
+        <Heading>Teste</Heading>
+      </Container>
+
+      <Container>
+        <Heading>Teste</Heading>
+      </Container>
     </>
   );
 }
