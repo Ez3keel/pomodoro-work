bd2b7b2914fc85d2fe96b076bce5fda6c687412d
Ezequiel <ezequiel.zunesha@gmail.com>
2025-04-28 18:01:08 -0300

    center elements

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@
 import "./styles/theme.css";
 import "./styles/global.css";
 
-
 import { Container } from "./components/Container";
 import { Heading } from "./components/Heading";
+import { Logo } from "./components/Logo";
 
 //Se exportar direto na function quando for importar no main tem que colocar o mesmo nome no import import Ex.: { App } from "./App";
 export function App() {
   return (
     <>
       <Container>
-        <Heading>LOGO</Heading>
+        <Logo />
       </Container>
 
       <Container>
