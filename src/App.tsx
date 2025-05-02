263b9f3184581538e139e9a4ea1f1920fe740461
Ezequiel <ezequiel.zunesha@gmail.com>
2025-05-02 17:45:31 -0300

    adc pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,62 +2,11 @@
 import "./styles/theme.css";
 import "./styles/global.css";
 
-import { Container } from "./components/Container";
-import { Logo } from "./components/Logo";
-import { Menu } from "./components/Menu";
-import { CountDown } from "./components/CountDown";
-import { DefaultInput } from "./components/DefaultInput";
-import { Cycles } from "./components/Cycles";
-import { DefaultButton } from "./components/DefaultButton";
-import { Footer } from "./components/Footer";
-import { PlayCircleIcon } from "lucide-react";
+import { Home } from "./pages/Home";
+import { NotFound } from "./pages/NotFound";
+import { AboutPomodoro } from "./pages/AboutPomodoro";
 
 //Se exportar direto na function quando for importar no main tem que colocar o mesmo nome no import import Ex.: { App } from "./App";
 export function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <form className="form" action="">
-          <div className="formRow">
-            <DefaultInput
-              labelText="task"
-              id="meuInput"
-              type="text"
-              title="TITULO"
-              placeholder="Digite algo"
-            />
-          </div>
-          <div className="formRow">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className="formRow">
-            <Cycles />
-          </div>
-
-          <div className="formRow">
-            <DefaultButton color="green" icon={<PlayCircleIcon />} />
-          </div>
-        </form>
-      </Container>
-
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
-
-// export { App };
